afe608a3dfaa4e8ef8e43db9ec0014dddfcdd464
= <larypetero@gmail.com>
2021-04-22 02:09:07 +0100

    quick fix

--- modern/app.php
+++ modern/app.php
@@ -14,7 +14,7 @@
     <head>
 
         <meta charset="utf-8" />
-        <title>Dashboard | UBold - Responsive Admin Dashboard Template</title>
+        <title>Dashboard | Revenue</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
         <meta content="Coderthemes" name="author" />
