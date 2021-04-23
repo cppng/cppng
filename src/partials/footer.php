9c6c9fea5f835203520f48e6d38f1583f6a8ed8e
Ndubuisi Samuel <samniwebdev@gmail.com>
2021-04-23 09:49:09 +0100

    route cummulative

--- src/partials/footer.php
+++ src/partials/footer.php
@@ -30,19 +30,19 @@
         <!-- END wrapper -->
 
         <!-- Vendor js -->
-        <script src="./assets/js/vendor.min.js"></script>
+        <script src="/assets/js/vendor.min.js"></script>
 
         <!-- Plugins js-->
-        <script src="./assets/libs/flatpickr/flatpickr.min.js"></script>
-        <script src="./assets/libs/apexcharts/apexcharts.min.js"></script>
+        <script src="/assets/libs/flatpickr/flatpickr.min.js"></script>
+        <script src="/assets/libs/apexcharts/apexcharts.min.js"></script>
 
-        <script src="./assets/libs/selectize/js/standalone/selectize.min.js"></script>
+        <script src="/assets/libs/selectize/js/standalone/selectize.min.js"></script>
 
         <!-- Dashboar 1 init js-->
-        <script src="./assets/js/pages/dashboard-1.init.js"></script>
+        <script src="/assets/js/pages/dashboard-1.init.js"></script>
 
         <!-- App js-->
-        <script src="./assets/js/app.min.js"></script>
+        <script src="/assets/js/app.min.js"></script>
         
     </body>
 </html>
