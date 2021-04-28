b8e78beca5e8b15cf782dabe7f816b229fbae3a4
nanipaul68@gmail.com <nanipaul68@gmail.com>
2021-04-28 16:16:04 +0100

    update

--- src/pages/igr-heroes/index.php
+++ src/pages/igr-heroes/index.php
@@ -153,7 +153,7 @@
 </div>
 
     
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous"></script>
+    
     <script>
         $(document).ready(function() {
             $('#hero_month').on('change', function () {
--- src/partials/footer.php
+++ src/partials/footer.php
@@ -31,6 +31,7 @@
 
         <!-- Vendor js -->
         <script src="/assets/js/vendor.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous"></script>
 
         <!-- Plugins js-->
         <script src="/assets/libs/flatpickr/flatpickr.min.js"></script>
